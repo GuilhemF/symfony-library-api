92c143b12f8c48df398a4118c16465e01835fefd
GuilhemF <contact@guilhemf.com>
2025-08-14 12:16:14 +0200

    pas de clef dans le git :zap:

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Bienvenue sur le dépôt de notre application [Symfony 7.3](https://symfony.com/) avec [API Platform 4.1](https://api-platform.com/docs/symfony/). Ce document vous guidera pour lancer le projet en local.
 
+## Prérequis
+
+PHP 8.4.7
+
 ## Installation
 
 1.  **Clonez le dépôt** :
@@ -12,8 +16,17 @@ Bienvenue sur le dépôt de notre application [Symfony 7.3](https://symfony.com/
     ```
 
 2.  **Installez les dépendances Composer** :
+
     ```bash
     composer install
+    ```
+
+3.  **Générer les clés de sécurité** :
+
+    Cette commande crée les clés `private.pem` et `public.pem` nécessaires pour signer les tokens d'authentification JWT.
+
+    ```bash
+    php bin/console lexik:jwt:generate-keypair
     ```
 
 ## Lancement du serveur
---
+++ migrations/Version20250811072536.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250811072536 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE auteur (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE categorie (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE livre_categorie (livre_id INT NOT NULL, categorie_id INT NOT NULL, INDEX IDX_E61B069E37D925CB (livre_id), INDEX IDX_E61B069EBCF5E72D (categorie_id), PRIMARY KEY(livre_id, categorie_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE livre_categorie ADD CONSTRAINT FK_E61B069E37D925CB FOREIGN KEY (livre_id) REFERENCES livre (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE livre_categorie ADD CONSTRAINT FK_E61B069EBCF5E72D FOREIGN KEY (categorie_id) REFERENCES categorie (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE livre ADD auteur_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE livre ADD CONSTRAINT FK_AC634F9960BB6FE6 FOREIGN KEY (auteur_id) REFERENCES auteur (id)');
+        $this->addSql('CREATE INDEX IDX_AC634F9960BB6FE6 ON livre (auteur_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE livre DROP FOREIGN KEY FK_AC634F9960BB6FE6');
+        $this->addSql('ALTER TABLE livre_categorie DROP FOREIGN KEY FK_E61B069E37D925CB');
+        $this->addSql('ALTER TABLE livre_categorie DROP FOREIGN KEY FK_E61B069EBCF5E72D');
+        $this->addSql('DROP TABLE auteur');
+        $this->addSql('DROP TABLE categorie');
+        $this->addSql('DROP TABLE livre_categorie');
+        $this->addSql('DROP INDEX IDX_AC634F9960BB6FE6 ON livre');
+        $this->addSql('ALTER TABLE livre DROP auteur_id');
+    }
+}
